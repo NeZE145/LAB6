a242ac27ea63d660da2421af014d675debb7a4f7
NeZE <boss-NeZE@hotmail.com>
2021-11-26 17:24:30 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i=1;int a=0;int b=0;
+    
+    while(i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i != 0){
+        if(i%2 ==0){
+            a=a+1;
+        }else{
+            
+            b=b+1;
+        }
+        }
+
+    }
+    
+    cout << "#Even numbers = " << a;
+    cout << '\n';
+    cout << "#odd numbers = " << b;
     return 0;
 }
